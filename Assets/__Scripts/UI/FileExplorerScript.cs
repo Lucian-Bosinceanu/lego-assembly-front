cebfc849cf47a554888679015b5b953c483c4889
Monica Ivn <monicaivan25@gmail.com>
2018-12-05 19:20:56 +0200

    Added script for opening file explorer and importing json file, with dialog popul displaying the json

---
+++ Assets/__Scripts/UI/FileExplorerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class FileExplorerScript : MonoBehaviour
+{
+    public InputField pathToJson;
+
+    public void OpenExplorer()
+    {
+        pathToJson.text = EditorUtility.OpenFilePanel("Overwrite with json", "", "json");
+    }
+
+    public void ImportJson()
+    {
+        if (pathToJson.text != null)
+        {
+            string dataAsJson = File.ReadAllText(pathToJson.text);
+
+            //TODO: remove line below and use dataAsJson to import structure object
+            EditorUtility.DisplayDialog("Your json:", dataAsJson, "Ok");
+        }
+    }
+}
