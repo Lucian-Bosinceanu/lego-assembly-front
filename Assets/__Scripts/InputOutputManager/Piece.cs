bf1eef8b568e9cc375756112c5b595f7805247ad
ginjucristian <ginjucristi@gmail.com>
2018-12-17 14:09:20 +0200

    Implemented InputOutputManager

---
+++ Assets/__Scripts/InputOutputManager/Piece.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace InputOutputManager
+{
+    [Serializable]
+    class Piece
+    {
+        public String name;
+        public Vector3 structure;
+    }
+}
