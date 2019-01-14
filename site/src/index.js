63ea75f603ffd77adc84e4c302d2a533f4d8e1f4
Lucian Bosinceanu <lucian.a.bosinceanu@gmail.com>
2019-01-14 22:31:25 +0200

    Added hash router

--- site/src/index.js
+++ site/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { App }  from './app/components/App.js'
 
 import "./media/styles.css"
 
 render((
-    <BrowserRouter>
+    <HashRouter>
             <App />
-    </BrowserRouter>
+    </HashRouter>
 ), window.document.getElementById('root'));
