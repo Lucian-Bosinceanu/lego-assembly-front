977906e60d56353fbdbbb23dc214a4d7d56c19a8
Rafael <rafaelmartonca2@gmail.com>
2019-01-14 19:30:16 +0200

    Modificari afisare

--- Assets/__Scripts/AnimationAndExportAlgAndSolutionPresent/animatiePrezentare.cs
+++ Assets/__Scripts/AnimationAndExportAlgAndSolutionPresent/animatiePrezentare.cs
@@ -8,16 +8,10 @@ public class animatiePrezentare : MonoBehaviour
     public GameObject importButton;
     private InputCollection inputCollection;
     public int button;
-    int i,speed;
+    int i;
+    float speed;
     public Vector3[] coord = new Vector3[5000];
-    // Use this for initialization
- 
 
-    public void PrezentareSolutie()
-    {
-        SpawnCubes();
-        speed = 2;
-    }
 
     public void ChangeSpeedToEnd()
     {
@@ -33,135 +27,135 @@ public class animatiePrezentare : MonoBehaviour
     //{
     //    speed = 2;
     //}
-
-
-    public void SpawnCubes()
+    public void spawn()
     {
-        inputCollection = importButton.GetComponent<InputLoader>().inputCollection;
-        Debug.Log("asd");
-        for (i=1;i<=5000;i++)
-            {
-
-            //Camera camera = GetComponent<Camera>();
-
-            StartCoroutine(MyCoroutine());
-
-            //camera.transform.position = new Vector3(inputCollection.order[i].position.x,inputCollection.order[i].position.y + 1, inputCollection.order[i].position.z - 7);//mutare camera pe cubul care trebuie as apara
-
-            if (inputCollection.order[i].name == "1x1")
-            {
-                cub1x1(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
-            }
-
-            if (inputCollection.order[i].name == "1x2")
-            {
-                cub1x2(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
-            }
-
-            if (inputCollection.order[i].name == "1x3")
-            {
-                cub1x3(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
-            }
-
-            if (inputCollection.order[i].name == "1x4")
-            {
-                cub1x4(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
-            }
-
-            if (inputCollection.order[i].name == "2x2")
-            {
-                cub2x2(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
-            }
-
-            if (inputCollection.order[i].name == "2x3")
-            {
-                cub2x3(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
-            }
-
-            if (inputCollection.order[i].name == "2x4")
-            {
-                cub2x4(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
-            }
-
-        }
 
     }
 
-    IEnumerator MyCoroutine()
+    public void SpawnCubes()
     {
+
+        speed = 2;
+        StartCoroutine(asd());
+        
+    }
+
+    IEnumerator asd()
+    {
+        inputCollection = importButton.GetComponent<InputLoader>().inputCollection;
+        for (i = 1; i <= 5000; i++)
+        {
+        Debug.Log("wait_on");
         yield return new WaitForSeconds(speed);
+        Debug.Log("wait_off");
+
+        if (inputCollection.order[i].name == "1x1")
+        {
+            Debug.Log("spawned 1x1");
+            cub1x1(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
+        }
+
+        if (inputCollection.order[i].name == "1x2")
+        {
+            Debug.Log("spawned 1x2");
+            cub1x2(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
+        }
+
+        if (inputCollection.order[i].name == "1x3")
+        {
+            Debug.Log("spawned 1x3");
+            cub1x3(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
+        }
+
+        if (inputCollection.order[i].name == "1x4")
+        {
+            Debug.Log("spawned 1x4");
+            cub1x4(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
+        }
+
+        if (inputCollection.order[i].name == "2x2")
+        {
+            Debug.Log("spawned 2x2");
+            cub2x2(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
+        }
+
+        if (inputCollection.order[i].name == "2x3")
+        {
+            Debug.Log("spawned 2x3");
+            cub2x3(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
+        }
+
+        if (inputCollection.order[i].name == "2x4")
+        {
+            Debug.Log("spawned 2x4");
+            cub2x4(inputCollection.order[i].position.x, inputCollection.order[i].position.y, inputCollection.order[i].position.z, inputCollection.order[i].rotation);//crearea cubului
+        }
+    }
     }
 
     //functiile pentru crearea cuburilor pe dimensiuni
 
-    void cub1x1(float x, float y, float z,int rotation)
+    void cub1x1(float x, float y, float z, int rotation)
     {
-        GameObject cube1x1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObject cube1x1 = Instantiate(GameObject.Find("Cubes/1x1"));
         cube1x1.transform.position = new Vector3(x, y + 20, z);
-        cube1x1.transform.Rotate(cube1x1.transform.eulerAngles.x, cube1x1.transform.eulerAngles.y-rotation, cube1x1.transform.eulerAngles.z);
-        //cube1x1.material.color = Color.blue;
-        cube1x1.AddComponent<Rigidbody>();
-    } 
+        cube1x1.transform.Rotate(cube1x1.transform.eulerAngles.x, cube1x1.transform.eulerAngles.y - rotation, cube1x1.transform.eulerAngles.z);
+
+    }
 
     void cub1x2(float x, float y, float z, int rotation)
     {
-        GameObject cube1x2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube1x2.transform.position = new Vector3(x, y+20, z);
+        GameObject cube1x2 = Instantiate(GameObject.Find("Cubes/1x2"));
+        cube1x2.transform.position = new Vector3(x, y + 20, z);
         cube1x2.transform.Rotate(cube1x2.transform.eulerAngles.x, cube1x2.transform.eulerAngles.y - rotation, cube1x2.transform.eulerAngles.z);
-        cube1x2.transform.localScale = new Vector3(cube1x2.transform.localScale.x*2, cube1x2.transform.localScale.y, cube1x2.transform.localScale.z);//modificarea cubului standard pentru 
-        cube1x2.AddComponent<Rigidbody>();
+        
     }
 
     void cub1x3(float x, float y, float z, int rotation)
     {
-        GameObject cube1x3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObject cube1x3 = Instantiate(GameObject.Find("Cubes/1x3"));
         cube1x3.transform.position = new Vector3(x, y + 20, z);
         cube1x3.transform.Rotate(cube1x3.transform.eulerAngles.x, cube1x3.transform.eulerAngles.y - rotation, cube1x3.transform.eulerAngles.z);
-        cube1x3.transform.localScale = new Vector3(cube1x3.transform.localScale.x * 3, cube1x3.transform.localScale.y, cube1x3.transform.localScale.z);
-        cube1x3.AddComponent<Rigidbody>();
+
     }
 
     void cub1x4(float x, float y, float z, int rotation)
     {
-        GameObject cube1x4 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObject cube1x4 = Instantiate(GameObject.Find("Cubes/1x4"));
         cube1x4.transform.position = new Vector3(x, y + 20, z);
         cube1x4.transform.Rotate(cube1x4.transform.eulerAngles.x, cube1x4.transform.eulerAngles.y - rotation, cube1x4.transform.eulerAngles.z);
-        cube1x4.transform.localScale = new Vector3(cube1x4.transform.localScale.x * 4, cube1x4.transform.localScale.y, cube1x4.transform.localScale.z);
-        cube1x4.AddComponent<Rigidbody>();
+
     }
 
     void cub2x2(float x, float y, float z, int rotation)
     {
-        GameObject cube2x2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObject cube2x2 = Instantiate(GameObject.Find("Cubes/2x2"));
         cube2x2.transform.position = new Vector3(x, y + 20, z);
         cube2x2.transform.Rotate(cube2x2.transform.eulerAngles.x, cube2x2.transform.eulerAngles.y - rotation, cube2x2.transform.eulerAngles.z);
-        cube2x2.transform.localScale = new Vector3(cube2x2.transform.localScale.x * 2, cube2x2.transform.localScale.y *2 , cube2x2.transform.localScale.z);
-        cube2x2.AddComponent<Rigidbody>();
+
     }
 
     void cub2x3(float x, float y, float z, int rotation)
     {
-        GameObject cube2x3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObject cube2x3 = Instantiate(GameObject.Find("Cubes/2x3"));
         cube2x3.transform.position = new Vector3(x, y + 20, z);
         cube2x3.transform.Rotate(cube2x3.transform.eulerAngles.x, cube2x3.transform.eulerAngles.y - rotation, cube2x3.transform.eulerAngles.z);
-        cube2x3.transform.localScale = new Vector3(cube2x3.transform.localScale.x * 3, cube2x3.transform.localScale.y * 2, cube2x3.transform.localScale.z);
-        cube2x3.AddComponent<Rigidbody>();
         cube2x3.GetComponent<Renderer>().material.color = new Color(4, 5, 2, 1);
-        
+
     }
 
     void cub2x4(float x, float y, float z, int rotation)
     {
-        GameObject cube2x4 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObject cube2x4 = Instantiate(GameObject.Find("Cubes/2x4"));
         cube2x4.transform.position = new Vector3(x, y + 20, z);
         cube2x4.transform.Rotate(cube2x4.transform.eulerAngles.x, cube2x4.transform.eulerAngles.y - rotation, cube2x4.transform.eulerAngles.z);
-        cube2x4.transform.localScale = new Vector3(cube2x4.transform.localScale.x * 4, cube2x4.transform.localScale.y * 2, cube2x4.transform.localScale.z);
-        cube2x4.AddComponent<Rigidbody>();
+
     }
 
 
     void Update()
     {
-        
+
     }
+
 }
